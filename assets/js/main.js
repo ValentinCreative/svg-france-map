72df19cb53b97d0effabf79033a364272913c65a
Valentin LÉVÊQUE <contact@valentinleveque.me>
2014-09-29 21:58:09 +0200

    Hover effects

--- assets/js/main.js
+++ assets/js/main.js
@@ -27,7 +27,41 @@ $.fn.highlightSVG = function(element,className){
     return this;
 };
 (function($){
-    var $idfLarge = $('#ile-de-france-zoomed');
+    'use strict';
+
+    var $mapContainer  = $('#map-container'),
+        $idfLarge      = $('#ile-de-france-zoomed'),
+        $path          = $('path, #dom-tom g'),
+        $tooltip       = $('<span/>', {
+            class: 'tooltip',
+        });
 
     $('#ile-de-france-min').highlightSVG($idfLarge, 'highlight');
+
+    $('input[name=map-type]', '#map-type').on('change', function() {
+       var mapType = $(this).val();
+
+       $mapContainer.attr('class', mapType);
+    });
+
+    $path.on('mouseover', function(evt) {
+        var $this = $(this),
+            name  = $this.data('name');
+
+        if (name != '') {
+            $mapContainer.append($tooltip);
+
+            $tooltip.show().text(name).css({
+                position   : 'absolute',
+                top        : evt.pageY,
+                left       : evt.pageX + 30
+            });
+        };
+
+    });
+
+    $path.on('mouseout', function() {
+        $tooltip.remove();
+    });
+
 })(jQuery);
--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,39 @@
 (function($){
-    var $idfLarge = $('#ile-de-france-zoomed');
+    'use strict';
+
+    var $mapContainer  = $('#map-container'),
+        $idfLarge      = $('#ile-de-france-zoomed'),
+        $path          = $('path, #dom-tom g'),
+        $tooltip       = $('<span/>', {
+            class: 'tooltip',
+        });
 
     $('#ile-de-france-min').highlightSVG($idfLarge, 'highlight');
+
+    $('input[name=map-type]', '#map-type').on('change', function() {
+       var mapType = $(this).val();
+
+       $mapContainer.attr('class', mapType);
+    });
+
+    $path.on('mouseover', function(evt) {
+        var $this = $(this),
+            name  = $this.data('name');
+
+        if (name != '') {
+            $mapContainer.append($tooltip);
+
+            $tooltip.show().text(name).css({
+                position   : 'absolute',
+                top        : evt.pageY,
+                left       : evt.pageX + 30
+            });
+        };
+
+    });
+
+    $path.on('mouseout', function() {
+        $tooltip.remove();
+    });
+
 })(jQuery);
